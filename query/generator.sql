728ff20aa59990cc679d8cc1ad74d65bbd50b68f
jumper423 <jump.e.r@yandex.ru>
2016-10-12 21:06:44 +0300

    generate

---
+++ query/generator.sql
@@ -0,0 +1,40 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS  `genetaror`;
+CREATE PROCEDURE `genetaror` (IN genCount INT)
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE queryString TEXT;
+    DECLARE queryInsert TEXT;
+
+    SET queryString = '';
+    WHILE i < genCount DO
+
+      SET queryInsert = CONCAT(
+          '(\'',
+           SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '\',',
+           MOD(i, 2),',\'',
+           CONCAT(SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '@', SUBSTRING(MD5(RAND()) FROM 1 FOR 3), '.com'),
+           '\')'
+      );
+      IF MOD(i, 1000) = 0
+        THEN
+          SET @queryString = CONCAT('INSERT INTO `users`(
+                                    `name`,
+                                    `gender`,
+                                    `email`
+                                    ) VALUES ',
+                                    queryString, queryInsert);
+          PREPARE queryPrePare FROM @queryString;
+          EXECUTE queryPrePare;
+          SET queryString = '';
+        ELSE
+          SET queryString = CONCAT(queryString, queryInsert, ',');
+        END IF;
+        SET i = i + 1;
+    END WHILE;
+END//
+
+DELIMITER ;
+
+CALL genetaror(10000000);
