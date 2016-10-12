6f0a8afa881c6ac9dd90cc63b6f38624c51817ac
jumper423 <jump.e.r@yandex.ru>
2016-10-12 21:16:44 +0300

    generate

--- query/generator.sql
+++ query/generator.sql
@@ -2,7 +2,7 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS  `genetaror`;
 CREATE PROCEDURE `genetaror` (IN genCount INT)
-BEGIN
+  BEGIN
     DECLARE i INT DEFAULT 1;
     DECLARE queryString TEXT;
     DECLARE queryInsert TEXT;
@@ -12,28 +12,31 @@ BEGIN
 
       SET queryInsert = CONCAT(
           '(\'',
-           SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '\',',
-           MOD(i, 2),',\'',
-           CONCAT(SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '@', SUBSTRING(MD5(RAND()) FROM 1 FOR 3), '.com'),
-           '\')'
+          SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '\',',
+          MOD(i, 2),',\'',
+          CONCAT(SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '@', SUBSTRING(MD5(RAND()) FROM 1 FOR 3), '.com'),',',
+          CONCAT(SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '@', SUBSTRING(MD5(RAND()) FROM 1 FOR 3), '.com'),',',
+          CONCAT(SUBSTRING(MD5(RAND()) FROM 1 FOR 6), '@', SUBSTRING(MD5(RAND()) FROM 1 FOR 3), '.com'),
+          '\'',
+          ')'
       );
       IF MOD(i, 1000) = 0
-        THEN
-          SET @queryString = CONCAT('INSERT INTO `users`(
+      THEN
+        SET @queryString = CONCAT('INSERT INTO `users`(
                                     `name`,
                                     `gender`,
                                     `email`
                                     ) VALUES ',
-                                    queryString, queryInsert);
-          PREPARE queryPrePare FROM @queryString;
-          EXECUTE queryPrePare;
-          SET queryString = '';
-        ELSE
-          SET queryString = CONCAT(queryString, queryInsert, ',');
-        END IF;
-        SET i = i + 1;
+                                  queryString, queryInsert);
+        PREPARE queryPrePare FROM @queryString;
+        EXECUTE queryPrePare;
+        SET queryString = '';
+      ELSE
+        SET queryString = CONCAT(queryString, queryInsert, ',');
+      END IF;
+      SET i = i + 1;
     END WHILE;
-END//
+  END//
 
 DELIMITER ;
 
